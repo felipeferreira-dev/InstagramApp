5c10b942627d93e30257c6648a7a771002eb7aeb
felipeferreira_dev <dev.felipeferreira@gmail.com>
2021-03-16 13:30:28 -0300

    First commit

---
+++ app/src/main/java/br/com/dev/felipeferreira/instargramremake/login/presentation/LoginActivity.java
@@ -0,0 +1,16 @@
+package br.com.dev.felipeferreira.instargramremake.login.presentation;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+import br.com.dev.felipeferreira.instargramremake.R;
+
+public class LoginActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_login);
+    }
+}
